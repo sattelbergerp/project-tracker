371ef01b49ac78098ed95cdb2ddf4f58981846f5
vbox <sattelbergerp@gmail.com>
2017-05-16 19:47:57 -0400

    Add /login GET & POST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,18 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id;
+      redirect "/projects"
+    else
+      redirect "/login"
+    end
+  end
+
 end
