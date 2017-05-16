e268449fc81b09b8d382d7f4d8ed288c469397b2
vbox <sattelbergerp@gmail.com>
2017-05-16 19:35:05 -0400

    Add /signup GET & POST to ApplicationController, enable sessions & set secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,26 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :session
+    set :session_secret, "we5gtr7hyu8i"
   end
 
   get "/" do
-    "Project Tracker"
+    erb :index
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    user = User.new(params[:user])
+    if user.save
+      session[:user_id] = user.id;
+      redirect "/projects"
+    else
+      redirect "/signup"
+    end
   end
 
 end
