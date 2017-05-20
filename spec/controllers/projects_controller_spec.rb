0cb203c736153b1f03b3966e36d44915bbc9b88a
vbox <sattelbergerp@gmail.com>
2017-05-20 19:41:03 -0400

    Add test for project index not showing other users projects

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -15,6 +15,16 @@ describe ProjectsController do
         expect(page).to have_content(project.name)
       end
     end
+    it "does not list other users projects" do
+      user = User.create(name:"user", email: "mail", password: "pass")
+      user2 = create_and_login_user('user','pass')
+      projects = [user.projects.create(name:"Test_Project_1"), user.projects.create(name:"Test_Project_2")]
+
+      visit "/projects"
+      projects.each do |project|
+        expect(page).not_to have_content(project.name)
+      end
+    end
     it "redirects the user to the homepage when not logged in" do
       visit "/projects"
       expect(page).to have_current_path("/")
